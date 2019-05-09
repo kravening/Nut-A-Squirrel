cd48c900940769dc8aa3c661367d4573191e6f61
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-05-09 10:28:00 +0200

    Forgot to transfer files from the work copy of this project to the repository folder. that means, the comment from the previous commit is applicable to this commit.

---
+++ Assets/_Scripts/SquirrelController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquirrelController : MonoBehaviour
+{
+    [SerializeField]private TreeRuffleBehaviour treeRuffleBehaviour;
+    private bool _isSquirrelHidden = true;
+    private bool _isHiding = false;
+    private Animator _animator;
+
+    public void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Show()
+    {
+        if (_isSquirrelHidden)
+        {
+            _isSquirrelHidden = false;
+            SquirrelManager.instance?.SquirrelShown();
+            StartCoroutine(ShowSquirrelRoutine());
+        }
+    }
+
+    private IEnumerator ShowSquirrelRoutine()
+    {
+        treeRuffleBehaviour?.RuffleTree();
+        yield return new WaitForSeconds(2);
+
+        _animator?.SetBool("IsShowing", true);
+        yield return new WaitForSeconds(2);
+
+        //if squirrel isn't already hidden or starting to hide.
+        if (_isSquirrelHidden == false && _isHiding == false)
+        {
+            Hide();
+        }
+    }
+
+    public void Hide()
+    {
+        SquirrelManager.instance?.SquirrelHiding();
+        StartCoroutine(HideSquirrelRoutine());
+    }
+
+    private IEnumerator HideSquirrelRoutine()
+    {
+        _isHiding = true;
+        _animator?.SetBool("IsShowing", false);
+        yield return new WaitForSeconds(0.5f);
+        _isSquirrelHidden = true;
+        _isHiding = false;
+    }
+}
