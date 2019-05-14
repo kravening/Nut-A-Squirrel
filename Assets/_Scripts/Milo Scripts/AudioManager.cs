ca7e002234dee75808384e68308b6556b86869fb
MrElektronix <milovanpelt@gmail.com>
2019-05-14 11:02:08 +0200

    Added audiomanager for playing sound (this sound can overlap)

---
+++ Assets/_Scripts/Milo Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+/// <summary>
+/// This class handles incoming audio events
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+   public AudioSource _audioSource;
+
+   /// <summary>
+   /// This function plays an audio file with the chosen volume
+   /// </summary>
+   /// <param name="audioClip"></param>
+   /// <param name="volume"></param>
+   public void PlayAudio(AudioClip audioClip, float volume)
+   {
+      _audioSource.clip = audioClip;
+      _audioSource.PlayOneShot(audioClip, volume);
+   }
+}
