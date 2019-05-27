5ed286993a56329da26f3c2eae2e1f4421cb4639
jeroen avis <Jeroenavis123@gmail.com>
2019-05-27 11:40:06 +0200

    Kleine fix

--- Assets/_Scripts/UIController.cs
+++ Assets/_Scripts/UIController.cs
@@ -8,13 +8,7 @@ public class UIController : MonoBehaviour
 {
 	[SerializeField] private TextMeshProUGUI highScoreText;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		UpdateScoreUi(4);
-	}
-
-	private void UpdateScoreUi(int score)
+	public void UpdateScoreUi(int score)
 	{
 		Debug.Log(score);
 		highScoreText.text = score.ToString();
