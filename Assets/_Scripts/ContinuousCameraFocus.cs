10859e8828e576aa7b0ed292a3ef2556ba698606
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-06-09 21:31:30 +0200

    Camera Auto focuses now.

---
+++ Assets/_Scripts/ContinuousCameraFocus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class ContinuousCameraFocus : MonoBehaviour
+{
+    void Start()
+    {
+        VuforiaARController vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            CameraDevice.Instance.SetFocusMode(CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
