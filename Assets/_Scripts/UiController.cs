ea075181a160b4e682d02aacbf61d87dc7e8d7ef
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-06-11 14:09:11 +0200

    CloudOrientationFixed

--- Assets/_Scripts/UiController.cs
+++ Assets/_Scripts/UiController.cs
@@ -1,52 +1,16 @@
-﻿using TMPro;
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-/// <summary>
-/// This controller handles all the UI elements of the game
-/// </summary>
-public class UiController : MonoBehaviour
+using UnityEngine.Serialization;
+
+public class UIController : MonoBehaviour
 {
-	/// <summary>
-	/// Gets the TextMeshProGui from the scene.
-	/// </summary>
-	[SerializeField] private TextMeshProUGUI[] highScoreText;
-	/// <summary>
-	/// makes the uicontroller an instance
-	/// </summary>
-	public static UiController instance;
+	[SerializeField] private TextMeshProUGUI highScoreText;
 
-	private void Awake()
-	{
-		if (instance != null && instance != this)
-		{
-			Destroy(this);
-		}
-		else
-		{
-			instance = this;
-		}
-	}
-
-	private void OnDestroy()
-	{
-		if (instance == this)
-		{
-			instance = null;
-		}
-	}
-	/// <summary>
-	/// Updates the score UI inside the game
-	/// </summary>
-	/// <param name="score"></param>
 	public void UpdateScoreUi(int score)
 	{
-		highScoreText[0].text = score.ToString();
-	}
-	/// <summary>
-	/// Updates the timer UI inside the game
-	/// </summary>
-	/// <param name="timer"></param>
-	public void TimerUi(int timer)
-	{
-		highScoreText[1].text = timer.ToString();
+		Debug.Log(score);
+		highScoreText.text = score.ToString();
 	}
 }
