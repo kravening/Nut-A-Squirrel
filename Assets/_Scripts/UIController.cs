077ba9e198239fb74271a0f1de9a9df509f7cc2e
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-06-11 14:23:47 +0200

    UIController fix Try#1

---
+++ Assets/_Scripts/UIController.cs
@@ -0,0 +1,53 @@
+﻿using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// This controller handles all the UI elements of the game
+/// </summary>
+public class UIController : MonoBehaviour
+{
+	/// <summary>
+	/// Gets the TextMeshProGui from the scene.
+	/// </summary>
+	[SerializeField] private TextMeshProUGUI[] highScoreText;
+	/// <summary>
+	/// makes the uicontroller an instance
+	/// </summary>
+	public static UIController instance;
+
+	private void Awake()
+	{
+		if (instance != null && instance != this)
+		{
+			Destroy(this);
+		}
+		else
+		{
+			instance = this;
+		}
+	}
+
+	private void OnDestroy()
+	{
+		if (instance == this)
+		{
+			instance = null;
+		}
+	}
+	/// <summary>
+	/// Updates the score UI inside the game
+	/// </summary>
+	/// <param name="score"></param>
+	public void UpdateScoreUI(int score)
+	{
+		highScoreText[0].text = score.ToString();
+	}
+	/// <summary>
+	/// Updates the timer UI inside the game
+	/// </summary>
+	/// <param name="timer"></param>
+	public void TimerUI(int timer)
+	{
+		highScoreText[1].text = timer.ToString();
+	}
+}
