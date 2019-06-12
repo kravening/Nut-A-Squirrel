b752fece498e5dfebdcd67b37ae20c45c55157b2
jeroen avis <Jeroenavis123@gmail.com>
2019-06-12 10:54:28 +0200

    HighScore endgame screen

--- Assets/_Scripts/UIController.cs
+++ Assets/_Scripts/UIController.cs
@@ -1,5 +1,6 @@
 ﻿using TMPro;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 /// <summary>
 /// This controller handles all the UI elements of the game
@@ -9,11 +10,15 @@ public class UIController : MonoBehaviour
 	/// <summary>
 	/// Gets the TextMeshProGui from the scene.
 	/// </summary>
-	[SerializeField] private TextMeshProUGUI[] highScoreText;
+	[SerializeField] private TextMeshProUGUI[] Textitems;
+
+	[SerializeField] private GameObject HighScoreUI;
 	/// <summary>
 	/// makes the uicontroller an instance
 	/// </summary>
 	public static UIController instance;
+
+	private int scoreText;
 
 	private void Awake()
 	{
@@ -33,21 +38,43 @@ public class UIController : MonoBehaviour
 		{
 			instance = null;
 		}
+		GameTimeManager.instance.GameEndedEvent -= HighScore;
+		GameTimeManager.instance.GameStartedEvent -= removeHighScoreUI;
 	}
+
+	private void Start()
+	{
+		GameTimeManager.instance.GameEndedEvent += HighScore;
+		GameTimeManager.instance.GameStartedEvent += removeHighScoreUI;
+	}
+
 	/// <summary>
 	/// Updates the score UI inside the game
 	/// </summary>
 	/// <param name="score"></param>
 	public void UpdateScoreUi(int score)
 	{
-		highScoreText[0].text = score.ToString();
+		scoreText = score;
+		Textitems[0].text = score.ToString();
 	}
+
+	private void removeHighScoreUI()
+	{
+		HighScoreUI.SetActive(false);
+	}
+
+	private void HighScore()
+	{
+		HighScoreUI.SetActive(true);
+		Textitems[2].text = scoreText.ToString();
+	}
+
 	/// <summary>
 	/// Updates the timer UI inside the game
 	/// </summary>
 	/// <param name="timer"></param>
 	public void TimerUi(int timer)
 	{
-		highScoreText[1].text = timer.ToString();
+		Textitems[1].text = timer.ToString();
 	}
 }
