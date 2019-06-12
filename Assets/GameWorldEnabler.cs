e370bb238c80d14edac11a78917213af5d251b3b
IntoVirtualDevelopment (Kaj) <development@intovisual.nl>
2019-06-12 10:19:15 +0200

    Trees done, and added a small script to enable / disable for when the the marker is in view or not.

---
+++ Assets/GameWorldEnabler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWorldEnabler : DefaultTrackableEventHandler
+{
+    public GameObject gameWorld;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        gameWorld.SetActive(true);
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        gameWorld.SetActive(false);
+    }
+}
